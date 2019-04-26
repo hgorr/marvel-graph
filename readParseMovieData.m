40a2f36cb36e9022de013733cb4a2ad9ca229d74
Heather Gorr, PhD <heather.meloy.gorr@gmail.com>
2019-04-26 09:01:07 -0400

    Updated import and parsing code

---
+++ readParseMovieData.m
@@ -0,0 +1,52 @@
+function [movieData,characterData,Movie] = readParseMovieData(apikey)
+% Read movie data with the OMDb API and character data from IMDb
+
+% Get movie and character data for each movie
+Movie = ["Iron Man";"The Incredible Hulk";"Iron Man 2";"Thor"; ...
+    "Captain America: The First Avenger";"The Avengers";"Iron Man 3";  ...
+    "Thor: The Dark World";"Captain America: The Winter Soldier";...
+    "Guardians of the Galaxy";"Avengers: Age of Ultron";"Ant-Man";...
+    "Captain America: Civil War";"Doctor Strange";"Guardians of the Galaxy Vol. 2";...
+    "Spider-Man: Homecoming";"Thor: Ragnarok";"Black Panther";...
+    "Avengers: Infinity War";"Ant-Man and the Wasp";"Captain Marvel";...
+    "Avengers: Endgame"];
+movieData = table;
+characterData = table('Size',[1,2],'VariableTypes',{'string','string'},...
+    'VariableNames',{'Character','Movie'});
+for ii = 1:length(Movie)
+    % Read movie data from OMDB API
+    OMDbURL = "http://www.omdbapi.com/?t="+Movie(ii)+apikey;
+    opts = weboptions('Timeout',200);
+    movieInfo = webread(OMDbURL,opts);
+    % Convert to table and add movie info
+    s = struct2table(movieInfo,'AsArray',true);
+    s = convertvars(s,[1:14,16:25],'string');
+    movieData = [movieData;s];                  %#ok
+    % Read full character list from IMDb
+    IMDbURL = "https://www.imdb.com/title/" + movieInfo.imdbID + ...
+        "/fullcredits?ref_=tt_cl_sm#cast";
+    w = webread(IMDbURL,opts);
+    % Parse html to get the list of characters in movie
+    w = string(extractBetween(w,'<td class="character">',"</td>"));
+    w = eraseBetween(w,"<",">","Boundaries","inclusive");
+    w = regexprep(w,"\s+"," ");
+    w = strip(w);
+    % Remove uncredited characters
+    w = unique(w);
+    Character = w(~contains(w,"(uncredited)"));
+    tnew = table(Character,repmat(Movie(ii),size(Character)),...
+        'VariableNames',{'Character','Movie'});
+    characterData = [characterData;tnew];       %#ok
+end
+
+% Convert to categorical
+characterData = convertvars(characterData,1:2,"categorical");
+% Clean up names and remove some movie characters from the list
+% For example, consider the following all the same name:
+%["Bruce Banner / The Hulk","Bruce Banner","Bruce Banner / Hulk"]
+MessyMarvelData
+
+% Remove missing data
+characterData = rmmissing(characterData);
+characterData.Character = removecats(characterData.Character);
+end
